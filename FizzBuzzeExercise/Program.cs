28be7c5d3bf62f0ce81f3ca67c12deae32bebd2c
patrick-atienza25 <patienza25@gmail.com>
2022-11-14 11:55:31 -0800

    finished

---
+++ FizzBuzzeExercise/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace FBuzzExercise
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FizzBuzz(5));
+        }
+
+        public static string FizzBuzz(int num)
+        {
+            //if (num % 5 == 0 && num % 3 == 0)
+            //    return "fizzbuzz";
+            //else if (num % 5 == 0)
+            //    return "buzz";
+            //else if (num % 3 == 0)
+            //    return "fizz";
+            //else
+            //    return "Not divisible by 3 or 5.";
+
+            //ternary refactor
+
+            var divisible = num % 3 == 0 && num % 5 == 0 ? "fizzbuzz" : num % 3 == 0 ? "fizz" : num % 5 == 0 ? "buzz" : "Not divisible by 3 or 5";
+            return divisible;
+        }
+    }
+
+}
